78d807e2f549907991fdb35770d491366aa8c0df
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-07-24 15:38:35 +0400

    Initial Commit

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Tako Menteshashvili on 24.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Tako Menteshashvili on 24.07.22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
